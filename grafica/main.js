72248860ab44ad8565571f619a63b115e6b74801
Jose Rodriguez <isc.joserodriguez@gmail.com>
2022-08-06 09:01:21 -0600

    Campos dinámicos

--- grafica/main.js
+++ grafica/main.js
@@ -1,35 +1,17 @@
+const inputCiudad = document.getElementById("nuevaCiudad");
+
 const ctx = document.getElementById("myChart").getContext("2d");
-const ciudades = ["Tepic", "Cancun", "Mexico", "Guerrero", 'Spain', 'England'];
+const ciudades = ["Tepic", "Cancun", "Mexico", "Guerrero", "Spain", "England"];
+let valoresTemp = [];
 
 const datasets = [
   {
     label: "Temperatura",
-    data: [],
+    data: [25, 30, 50, 42, 20, 100],
     backgroundColor: [
       "rgba(255, 99, 132, 0.2)",
       "rgba(54, 162, 235, 0.2)",
       "rgba(255, 206, 86, 0.2)",
-    ],
-    borderColor: [
-      "rgba(255, 99, 132, 1)",
-      "rgba(54, 162, 235, 1)",
-      "rgba(255, 206, 86, 1)",
-      "rgba(75, 192, 192, 1)",
-      "rgba(153, 102, 255, 1)",
-      "rgba(255, 159, 64, 1)",
-    ],
-    borderWidth: 1,
-  },
-  {
-    label: "Indice mortalidad",
-    data: [150, 200, 50, 0],
-    backgroundColor: [
-      "rgba(255, 99, 132, 0.2)",
-      "rgba(54, 162, 235, 0.2)",
-      "rgba(255, 206, 86, 0.2)",
-      "rgba(75, 192, 192, 0.2)",
-      "rgba(153, 102, 255, 0.2)",
-      "rgba(255, 159, 64, 0.2)",
     ],
     borderColor: [
       "rgba(255, 99, 132, 1)",
@@ -60,6 +42,7 @@ const getTemp = async () => {
   );
 
   datasets[0].data = valores;
+  valoresTemp = valores;
   myChart.update();
 };
 
@@ -69,6 +52,28 @@ const myChart = new Chart(ctx, {
     labels: ciudades,
     datasets,
   },
+});
+
+inputCiudad.addEventListener("keyup", async (e) => {
+  try{
+    if (e.code === "Enter") {
+      ciudades.push(e.target.value);
+      const {
+        data: {
+          main: { temp },
+        },
+      } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=616629f9acdc3b22b8b09553e632e5da`
+      );
+      console.log(valoresTemp);
+      valoresTemp.push(temp - 273.15);
+  
+      datasets[0].data = valoresTemp;
+      myChart.update();
+    }
+  }catch(e){
+    alert('Ingresa un valor válido')
+  }
 });
 
 getTemp();
